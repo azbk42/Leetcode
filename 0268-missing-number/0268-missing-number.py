6b6ca6072c9c8c3f0e6a40a60e2b0085227ccac7
Azbk <73858420+azbk42@users.noreply.github.com>
2023-09-20 14:01:26 +0200

    Time: 124 ms (67.26%), Space: 18.1 MB (13.38%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        num_set = set(nums)
+        for i in range(n + 1):
+            if i not in num_set:
+                return i
